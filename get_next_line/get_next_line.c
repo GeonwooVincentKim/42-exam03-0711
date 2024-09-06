19274aee43245a4e0cb70cdd45594fc1922e253d
Geonwoo Kim <48337874+GeonwooVincentKim@users.noreply.github.com>
2024-09-06 19:49:06 +0900

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,20 +12,43 @@
 
 #include "get_next_line.h"
 
-char	*ft_strdup(char *src)
+// char	*ft_strdup(char *src)
+// {
+// 	char	*dest;
+// 	int		i;
+
+// 	i = 0;
+// 	while (src[i])
+// 		i++;
+// 	dest = (char *)malloc(sizeof(char) * (i + 1));
+// 	i = -1;
+// 	while (src[++i])
+// 		dest[i] = src[i];
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+
+char	*ft_strdup(char *str)
 {
-	char	*dest;
-	int		i;
+	int	i;
+	int	len;
+	char *dup;
 
 	i = 0;
-	while (src[i])
-		i++;
-	dest = (char *)malloc(sizeof(char) * (i + 1));
-	i = -1;
-	while (src[++i])
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
+	len = 0;
+	while (str[len])
+		len++;
+	dup = malloc(sizeof(*str) * (len + 1));
+	if (dup != NULL)
+	{
+		while (str[i])
+		{
+			dup[i] = str[i];
+			i++;
+		}
+		dup[i] = '\0';
+	}
+	return (dup);
 }
 
 char	*get_next_line(int fd)
